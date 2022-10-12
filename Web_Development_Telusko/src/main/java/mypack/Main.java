d8b50e835aa57dab4ba3312440cb6dafc3fd8f43
nikil <nikil@DESKTOP-90FVEUC>
2022-10-12 23:44:32 +0530

    Clean

--- Web_Development_Telusko/src/main/java/mypack/Main.java
+++
@@ -1,9 +0,0 @@
-package mypack;
-
-public class Main {
-
-	public static void main(String[] args) {
-		System.out.println("New development/feature");
-		System.out.println("One more line as told by TL");
-	}
-}
--- Web_Development_Telusko/src/main/java/mypack/TrigCalculator.java
+++
@@ -1,8 +0,0 @@
-package mypack;
-
-public class TrigCalculator {
-// Add content here
-	//	Added successfully.
-	//	Are you sure???
-	//	Yes. I am preety fuckin' sure
-}
